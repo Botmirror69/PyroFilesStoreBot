369d12dcbdf934e6b79ca3e47b293644aa62782c
Botmirror69 <92679061+Botmirror69@users.noreply.github.com>
2022-09-22 22:56:44 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,13 +4,13 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
+	API_ID = int(os.environ.get("API_ID", "3477714"))
+	API_HASH = os.environ.get("API_HASH", "1264d2d7d397c4635147ee25ab5808d1")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "5732851333:AAEwH8IwA7HdtWFL-xuuALHFTJ0zaT9vpQ4")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "filesstorer_bot")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001593914815"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1773311819)
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://shikari:shikari@cluster0.srfww.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
