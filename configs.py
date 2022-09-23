9f2fc7ccc2f95ad6d9b51397f3a15cdab2796dad
Botmirror69 <92679061+Botmirror69@users.noreply.github.com>
2022-09-23 12:37:06 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ class Config(object):
 	BOT_TOKEN = os.environ.get("BOT_TOKEN", "5732851333:AAEwH8IwA7HdtWFL-xuuALHFTJ0zaT9vpQ4")
 	BOT_USERNAME = os.environ.get("BOT_USERNAME", "filesstorer_bot")
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001593914815"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1773311819")
+	BOT_OWNER = int(os.environ.get("BOT_OWNER")
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://shikari:shikari@cluster0.srfww.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
